c81fb331509b6f960d62a079d4138fa2b7c3f6bd
RakibAlom <rakibalom17@gmail.com>
2022-10-25 03:55:44 +0600

    install react-icons

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ function App() {
       <h1>Next-Learner: Future Tech Runner</h1>
       <h2>Install Firebase</h2>
       <h2>Install React-Router-DOM</h2>
+      <h2>Install React-Bootstrap</h2>
+      <h2>Install React-Icons</h2>
     </div>
   );
 }
